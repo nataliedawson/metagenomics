25c2d9af081736e5e460dab781744731a93b0c88
nataliedawson <nataliedawson@users.noreply.github.com>
2017-11-30 18:24:17 +0000

    Add clearer usage for script

--- extract_protein_sequences_using_id_list.pl
+++ extract_protein_sequences_using_id_list.pl
@@ -4,20 +4,59 @@
 
 use strict;
 use warnings;
-use Path::Class;
+use Path::Tiny;
 use Bio::SeqIO;
+use Getopt::Long;
 
-# check the number of arguments provided
-if ( scalar @ARGV < 3 ) {
-    print "Usage: $0 <FASTA_file> <IDs_file> <output_file>\n";
-    exit;
+my $USAGE = <<"_USAGE";
+Usage:
+$0 [required]
+
+Required:
+                --fasta                    FASTA file to be filtered
+                --ids_file                 Files of sequence ids to extract entries from FASTA
+                --fasta_out                FASTA file to contain the extracted sequences
+
+Example:
+$0 \
+   --fasta                       metagenome_contigs.faa \
+   --ids_file                    ids_of_interest.dat \
+   --fasta_out                   metagenome_contigs.extracted_genes_of_interest.faa
+
+Content example for --ids_file:
+1244082_1
+1244082_2
+1244082_3
+1244638_1
+1244638_2
+1244638_3
+
+Takes a FASTA file and extracts sequences using a specified list of ids.
+
+_USAGE
+
+sub USAGE {
+        print $USAGE;
+        print "Error: @_\n" if scalar @ARGV;
+        exit;
 }
 
+USAGE() unless scalar @ARGV;
+
 # user inputs
-my $file     = file( $ARGV[0] ); # FASTA file input
-my $ids_file = file( $ARGV[1] ); # list of ids file input
-my $out_file = file( $ARGV[2] ); # output file
-my $out_fh = $out_file->openw(); # open output file for writing
+my $file;      # FASTA file input
+my $ids_file;  # list of ids file input
+my $out_fh;    # output file
+
+GetOptions(
+    "fasta=s"         => \$file,
+    "ids_file=s"       => \$ids_file,
+    "fasta_out=s"     => sub{ $out_fh = path( $_[1] )->openw() },
+) or die( "Error in command line args.\n" );
+
+USAGE( "FASTA file to filter must be specified" ) unless defined $file;
+USAGE( "File of ids must be specified" ) unless defined $ids_file;
+USAGE( "Output file must be specified" ) unless defined $out_fh;
 
 print "WARN: Retrieving all id strings of interest and excluding any characters after whitespace, e.g. will extract '1244082_2' from '1244082_2 gene=1244082_2.gene'\n";
 my $ids_of_interest = parse_ids( $ids_file );
@@ -45,7 +84,6 @@ while(my $seq = $in->next_seq()) {
         my $sequence = $seq->seq;
 
         print "Matched sequence ID from list: $id\n";
-        #exit;
 
         # print out the id and sequence to file
         print $out_fh ">$id\n$sequence\n";
@@ -58,7 +96,7 @@ while(my $seq = $in->next_seq()) {
 sub parse_ids {
   my $ids_file = shift;
 
-  my @ids_from_file = $ids_file->slurp( chomp => 1 );
+  my @ids_from_file = path( $ids_file )->lines( { chomp => 1 } );
   my @ids_of_interest;
 
   foreach my $id ( @ids_from_file ) {
